71e3a1f5361600b8cf2eabd0475cf286249671e4
Alexandre Moleiro <avataranedotas@yahoo.com>
2021-06-27 12:38:45 +0100

    Update umap.sh

--- umap.sh
+++ umap.sh
@@ -12,9 +12,13 @@ echo "Type2_cable_0-22kW"
 wget -O Type2_cable_0-22kW.json 'https://z.overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea(3600295480)-%3E.searchArea%3B(node%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D(if%3A%20t%5B%22socket%3Atype2_cable%22%5D%20%3E%200)%20%5B%22socket%3Atype2_cable%3Aoutput%22~kW%5D%20(if%3A%20number%20(t%5B%22socket%3Atype2_cable%3Aoutput%22%5D)%20%3C%2023)%20(area.searchArea)%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B'
 sleep 60
 
+echo "Type2_cable_23-∞kW"
+wget -O Type2_cable_23-∞kW.json 'https://z.overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea(3600295480)-%3E.searchArea%3B(node%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D(if%3A%20t%5B%22socket%3Atype2_cable%22%5D%20%3E%200)%20%5B%22socket%3Atype2_cable%3Aoutput%22~kW%5D%20(if%3A%20number%20(t%5B%22socket%3Atype2_cable%3Aoutput%22%5D)%20%3E%2022%20)%20(area.searchArea)%3B)%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B'
+sleep 60
+
 sed -i '/.*timestamp_.*/d' *.json
 
 osmtogeojson Type2_0-10kW.json >| Type2_0-10kW.geojson
 osmtogeojson Type2_11-22kW.json >| Type2_11-22kW.geojson
 osmtogeojson Type2_cable_0-22kW.json >| Type2_cable_0-22kW.geojson
-
+osmtogeojson Type2_cable_23-∞kW.json >| Type2_cable_23-∞kW.geojson
