15d12dbd773227495f50d5ad8dfce8f63a586395
Alexandre Moleiro <avataranedotas@yahoo.com>
2024-03-30 19:07:16 +0000

    Update umap.sh

--- umap.sh
+++ umap.sh
@@ -3,20 +3,20 @@
 #Type2 e Type2_cable
 
 
-echo "Type2s"
-wget -O Type2s.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea(3600295480)-%3E.searchArea%3B%0A((%20%20nwr%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D%5B%22socket%3Atype2_combo%22!~%22.*%22%5D%5B%22socket%3Achademo%22!~%22.*%22%5D%0A%20%20(if%3A%20t%5B%22socket%3Atype2%22%5D%20%3E%200)%20%20%20%20(area.searchArea)%3B)%3B%0A(%20%20nwr%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D%5B%22socket%3Atype2_combo%22!~%22.*%22%5D%5B%22socket%3Achademo%22!~%22.*%22%5D%0A%20%20(if%3A%20t%5B%22socket%3Atype2_cable%22%5D%20%3E%200)%20%20%20%20(area.searchArea)%3B)%3B)%3B%0Aout%20center%3B%3E%3Bout%20skel%20qt%3B%0A'
+echo "PCN"
+wget -O Type2s.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea(3600295480)-%3E.searchArea%3B%0A(%20%20nwr%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D%5B%22socket%3Atype2_combo%22!~%22.*%22%5D%5B%22motorcar%22%3D%22yes%22%5D(area.searchArea)%3B)%3B%0Aout%20center%3B%3E%3Bout%20skel%20qt%3B%0A'
 sleep 60
 
 echo "PCR"
-wget -O PCR.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea(3600295480)-%3E.searchArea%3B%0A((%20%20nwr%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D(if%3A%20t%5B%22socket%3Atype2_combo%22%5D%20%3E%200)%20%5B%22socket%3Atype2_combo%3Aoutput%22~kW%5D%20(if%3A%20number%20(t%5B%22socket%3Atype2_combo%3Aoutput%22%5D)%20%3C%2076)%20(area.searchArea)%3B)%3B%0A(%20%20nwr%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D%5B%22socket%3Atype2_combo%22!~%22.*%22%5D(if%3A%20t%5B%22socket%3Achademo%22%5D%20%3E%200)%20%20%20%20(area.searchArea)%3B)%3B)%3B%0Aout%20center%3B%3E%3Bout%20skel%20qt%3B%0A'
+wget -O PCR.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea(3600295480)-%3E.searchArea%3B%0A(nwr%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D%5B%22socket%3Atype2_combo%22~%22.*%22%5D%20%5B%22socket%3Atype2_combo%3Aoutput%22~kW%5D%20(if%3A%20number%20(t%5B%22socket%3Atype2_combo%3Aoutput%22%5D)%20%3C%20150)%20(area.searchArea)%3B%0A)%3B%0Aout%20center%3B%3E%3Bout%20skel%20qt%3B'
 sleep 60
 
 echo "PCSR"
-wget -O PCSR.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea(3600295480)-%3E.searchArea%3B(nwr%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D(if%3A%20t%5B%22socket%3Atype2_combo%22%5D%20%3E%200)%20%5B%22socket%3Atype2_combo%3Aoutput%22~kW%5D%20(if%3A%20number%20(t%5B%22socket%3Atype2_combo%3Aoutput%22%5D)%20%3E%2074%20)(if%3A%20number%20(t%5B%22socket%3Atype2_combo%3Aoutput%22%5D)%20%3C%20151%20)%20(area.searchArea)%3B)%3Bout%20center%3B%3E%3Bout%20skel%20qt%3B'
+wget -O PCSR.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea(3600295480)-%3E.searchArea%3B%0A(nwr%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D%5B%22socket%3Atype2_combo%22~%22.*%22%5D%20%5B%22socket%3Atype2_combo%3Aoutput%22~kW%5D%20(if%3A%20number%20(t%5B%22socket%3Atype2_combo%3Aoutput%22%5D)%20%3E%2049)%20(if%3A%20number%20(t%5B%22socket%3Atype2_combo%3Aoutput%22%5D)%20%3C%20150)%20(area.searchArea)%3B)%3B%0Aout%20center%3B%3E%3Bout%20skel%20qt%3B'
 sleep 60
 
 echo "PCUR"
-wget -O PCUR.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea(3600295480)-%3E.searchArea%3B(nwr%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D(if%3A%20t%5B%22socket%3Atype2_combo%22%5D%20%3E%200)%20%5B%22socket%3Atype2_combo%3Aoutput%22~kW%5D%20(if%3A%20number%20(t%5B%22socket%3Atype2_combo%3Aoutput%22%5D)%20%3E150%20)%20(area.searchArea)%3B)%3Bout%20center%3B%3E%3Bout%20skel%20qt%3B'
+wget -O PCUR.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea(3600295480)-%3E.searchArea%3B%0A(nwr%5B%22amenity%22%3D%22charging_station%22%5D%5B%22access%22%3D%22yes%22%5D%5B%22socket%3Atype2_combo%22~%22.*%22%5D%20%5B%22socket%3Atype2_combo%3Aoutput%22~kW%5D%20(if%3A%20number%20(t%5B%22socket%3Atype2_combo%3Aoutput%22%5D)%20%3E%20349)%20(area.searchArea)%3B%0A)%3B%0Aout%20center%3B%3E%3Bout%20skel%20qt%3B'
 sleep 60
 
 echo "SuC"
